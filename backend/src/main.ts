672a11bd6c3c8fe2dff36690f459a9194a6bbb97
ValentinoJC <valenkxdd@gmail.com>
2024-08-11 22:19:40 -0300

    Fix en main y Docker final

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -14,14 +13,6 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe({}))
-
-  const config = new DocumentBuilder()
-  .setTitle('Cafetería La Esmeralda')
-  .setDescription('API de la aplicación de la cafetería La Esmeralda.')
-  .setVersion('1.0')
-  .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
