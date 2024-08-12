21bface5718a1840a9447226e3f1bb1cc2102829
Nico <nicolasaddamo1@gmail.com>
2024-08-12 13:17:16 -0300

    fix de storage image

--- backend/src/modules/storage/image.controller.ts
+++ backend/src/modules/storage/image.controller.ts
@@ -32,28 +32,3 @@ export class ImageController {
 
 }
 
-// @Controller('Storage')
-// export class StorageController {
-//   constructor(private readonly storageService: StorageService) {}
-
-//   @Post('upload')
-//   @ApiBearerAuth()
-//   @ApiOperation({
-//     summary: 'Añadir imagenes a la base de datos',
-//     description:
-//       'Esta ruta permite subir imagenes con los datos enviados por body',
-//   })
-//   @UseGuards(AuthGuard)
-//   @UseInterceptors(FileInterceptor('file'))
-//   async uploadImage(@UploadedFile(
-//     new ParseFilePipe({
-//       validators: [
-//         new MaxFileSizeValidator({ maxSize: 3000000, message: 'El archivo debe ser menor a 3mb.' }),
-//         new FileTypeValidator({ fileType: /(jpg|jpeg|png|jfif|webp)$/ })
-//       ]
-//     })
-//   ) file: Express.Multer.File): Promise<string> {
-//       return await this.storageService.uploadImage(file);
-//   }
-// }
-
