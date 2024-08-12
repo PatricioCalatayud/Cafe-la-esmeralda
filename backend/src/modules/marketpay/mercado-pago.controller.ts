7830c56657f5e59b6dec1404989c6debb9682c63
Nico <nicolasaddamo1@gmail.com>
2024-08-12 14:20:32 -0300

    Swagger de order listo

--- backend/src/modules/marketpay/mercado-pago.controller.ts
+++ backend/src/modules/marketpay/mercado-pago.controller.ts
@@ -1,11 +1,16 @@
 import { BadRequestException, Body, Controller, Get, Param, Post, Redirect, ValidationPipe } from "@nestjs/common";
 import { MercadoPagoService } from "./mercado-pago.service";
 import { CreatePaymentDto } from "./create-payment.dto";
-
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+@ApiTags('Pasarela de pago')
 @Controller('market-pay')
 export class MercadoPagoController {
     constructor(private readonly mercadoPagoService: MercadoPagoService) {}
-
+  @ApiOperation({
+    summary: 'Crea un pago en Mercado Pago',
+    description:
+        'Desde este endpoint se crea un link de pago a Mercado Pago.'
+  })
   @Post('url-proccess')
   async createPayment(@Body(new ValidationPipe()) createPaymentDto: CreatePaymentDto): Promise<any> {
     console.log('Received DTO:', createPaymentDto);
--- backend/src/modules/order/order.controller.ts
+++ backend/src/modules/order/order.controller.ts
@@ -1,37 +1,61 @@
 import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { AddOrderDto } from './order.dto';
-
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('Ordenes')
 @Controller('order')
 export class OrderController {
 
     constructor(
         private readonly orderService: OrderService,
     ){}
-
+    @ApiBearerAuth()
+    @ApiOperation({
+        summary:'Obtener ordenes',
+        description:'Obtiene todas las ordenes, tiene que estar registrado'
+    })
     @Get()
     async GetAll(){
         return await this.orderService.getAll()
     }   
-
+    @ApiOperation({
+        summary:'Obtener ordenes por id',
+        description:'Obtiene ordenes especificas, tiene que estar registrado'
+    })
+    @ApiBearerAuth()
     @Get(':id')
     async GetById(@Param('id', ParseUUIDPipe) id: string){
         return await this.orderService.getById(id)
         
     }
-
+    
     @Get('user/:id')
+    @ApiBearerAuth()
+    @ApiOperation({
+        summary:'Obtener ordenes por usuario',
+        description:'Obtiene ordenes especificas de un usuario, tiene que estar registrado'
+    })
     async GetByUserId(@Param('id', ParseUUIDPipe) id: string){
         return await this.orderService.getByUserId(id)
         
     }
-
+    
     @Post()
+    @ApiOperation({
+        summary:'Crear ordenes',
+        description:'Crea una nueva orden, tiene que estar registrado'
+    })
+    @ApiBearerAuth()
     async addOne(@Body() orderInfo:AddOrderDto){    
         const {userId,products,adress,cuponDescuento,deliveryDate} = orderInfo
         return await this.orderService.addOrder(userId,products,adress,Number(cuponDescuento),deliveryDate)
     }
-
+    
+    @ApiOperation({
+        summary:'Eliminar ordenes',
+        description:'Elimina una orden especifica, tiene que estar registrado'
+    })
+    @ApiBearerAuth()
     @Delete(':id')
     async deleteOne(){
         
