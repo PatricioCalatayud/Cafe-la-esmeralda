910242ab072d160f190d0d41ac1fb7ce747803c2
ValentinoJC <valenkxdd@gmail.com>
2024-08-11 21:32:42 -0300

    Fix en Docker e implementación de Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -12,12 +13,17 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe({
-  }
-  ))
+  app.useGlobalPipes(new ValidationPipe({}))
+
+  const config = new DocumentBuilder()
+  .setTitle('Cafetería La Esmeralda')
+  .setDescription('API de la aplicación de la cafetería La Esmeralda.')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
-  console.log(`Server listening on port ${port}`);
 }
 bootstrap();
