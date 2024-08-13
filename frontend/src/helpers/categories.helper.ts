1e8af25414ae313663aee1c46ddf72b0d7fb2811
Julian Magallanes <100171315+Julian-Magallanes@users.noreply.github.com>
2024-08-13 09:22:14 -0300

    arreglo de rutas

--- frontend/src/helpers/categories.helper.ts
+++ frontend/src/helpers/categories.helper.ts
@@ -5,7 +5,7 @@ const apiURL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getCategories(): Promise<Category[] | undefined> {
   try {
-    const res = await axios.get(`${apiURL}/category`);
+    const res = await axios.get(`${apiURL}/categories`);
     const categories: Category[] = res.data;
     return categories;
   } catch (error: any) {
@@ -23,7 +23,7 @@ export async function getProductsByCategory(
   categoryName: string
 ): Promise<IProductList[] | undefined> {
   try {
-    const res = await axios.get(`${apiURL}/products?category=${categoryName}`);
+    const res = await axios.get(`${apiURL}/products?categories=${categoryName}`);
     const products: IProductList[] = res.data;
     return products;
   } catch (error: any) {
