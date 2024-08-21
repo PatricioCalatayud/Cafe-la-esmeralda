c5f6ccb50a64a4e9b772142230cfb8f3c626ccc8
Julian Magallanes <100171315+Julian-Magallanes@users.noreply.github.com>
2024-08-20 22:23:14 -0300

    fix build

--- frontend/src/components/DashboardComponent/DashboardComponent.tsx
+++ frontend/src/components/DashboardComponent/DashboardComponent.tsx
@@ -60,7 +60,7 @@ const DashboardComponent = ({
             <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
             {buttonTopRight && 
             <div className="flex items-center space-x-3 w-full md:w-auto">
-          <Link
+          {buttonTopRightLink && <Link
             type="button"
             id="createProductButton"
             data-modal-toggle="createProductModal"
@@ -68,7 +68,7 @@ const DashboardComponent = ({
             href={buttonTopRightLink }
           >
             {buttonTopRight}
-          </Link>
+          </Link>}
         </div>}
 
             </div>
