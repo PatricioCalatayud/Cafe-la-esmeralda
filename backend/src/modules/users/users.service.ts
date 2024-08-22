8be0e48e769ae581d804ff4f188b45ccca49d368
ValentinoJC <valenkxdd@gmail.com>
2024-08-22 14:39:55 -0300

    Agregado getAllUsers

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,33 +1,42 @@
-import { Injectable, NotFoundException, ConflictException, BadRequestException} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import * as bcrypt from 'bcrypt';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
-import { JwtService } from '@nestjs/jwt';
-import { Role } from 'src/enum/roles.enum';
 import { UserDTO } from './users.dto';
-import { timeStamp } from 'console';
 
 @Injectable()
 export class UsersService {
-    constructor(
-    @InjectRepository(User) private userRepository: Repository<User>,
-    private readonly jwtService: JwtService,
-    ) {}
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+    async getUsers(page: number = 1, limit: number = 10): Promise<Partial<Omit<User, "password">[]>> {
+        const skip = (page - 1) * limit;
+      
+        const users = await this.userRepository.find({
+            skip,
+            take: limit,
+        });
+      
+        return users.map((user) => {
+            delete user.password;
+            return user;
+        })
+    }
 
     async getUserById(id: string): Promise<User | undefined> {
-    return await this.userRepository.findOne({ where: { id } });
+        return await this.userRepository.findOne({ where: { id } });
     }
 
     async updateUser(id: string, userDTO: Partial<UserDTO>): Promise<User | undefined> {
-    await this.userRepository.update(id, userDTO);
-    return await this.userRepository.findOne({ where: { id } });
+        await this.userRepository.update(id, userDTO);
+        return await this.userRepository.findOne({ where: { id } });
     }
 
-    async deleteUser(id: string): Promise<void> {
-    const result = await this.userRepository.delete(id);
-    if (result.affected === 0) {
-        throw new NotFoundException(`Usuario con ID ${id} no encontrado`);
+    async deleteUser(id: string) {
+        const result = await this.userRepository.delete(id);
+        if (result.affected === 0) {
+            throw new NotFoundException(`Usuario con ID ${id} no encontrado`);
         }
+
+        return { HttpCode: 200 };
     }
 }
