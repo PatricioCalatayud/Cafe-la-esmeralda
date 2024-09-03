fab9ca18887d196bfb4e8a91ecfc577ff07fae92
Nico <nicolasaddamo1@gmail.com>
2024-09-03 11:03:42 -0300

    changed controller

---
+++ backend/src/entities/ratings.entity.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./user.entity";
+
+@Entity('ratings')
+export class Rating{
+    @PrimaryGeneratedColumn('uuid')
+    id:string
+
+    @Column({type:'int', nullable:false})
+    rating:number
+
+    @CreateDateColumn()
+    created_at:Date
+
+    // @ManyToOne(()=>User, user => user.ratings)
+    // user:User
+
+    // @Many
+}
