1b5e9b3220c4493c2307459838ac7afa8b840aa5
Patricio Lucas Calatayud <patocalatayud@hotmail.com>
2024-09-05 15:57:18 -0300

    Calificacion de producto

--- frontend/src/app/dashboard/cliente/order/[id]/page.tsx
+++ frontend/src/app/dashboard/cliente/order/[id]/page.tsx
@@ -11,18 +11,20 @@ import { format } from "date-fns";
 import { es } from "date-fns/locale";
 import Image from "next/image";
 import RatingStars from "@/components/ratingStars/ratingStars";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const Tracking = ({ params }: { params: { id: string } }) => {
-  const { token, session } = useAuthContext();
+  const { token, session, userId } = useAuthContext();
   const [loading, setLoading] = useState(true);
-  const [order, setOrder] = useState<IOrders | null>(null); // Tipado correcto
-
+  const [order, setOrder] = useState<IOrders | null>(null);
+  const [ratings, setRatings] = useState<{ [key: string]: number }>({});
 
   useEffect(() => {
     async function fetchOrder() {
       try {
         const fetchedOrder = await getOrder(params.id, token);
-        setOrder(fetchedOrder as IOrders); // Establecer el estado con el pedido
+        setOrder(fetchedOrder as IOrders);
       } catch (error) {
         console.error("Error fetching order:", error);
       } finally {
@@ -33,11 +35,40 @@ const Tracking = ({ params }: { params: { id: string } }) => {
   }, [params.id, token]);
 
   const statusDefault = ["Recibido", "Empaquetado", "Transito", "Entregado"];
-  console.log(order, session?.id);
+  
+  const handleRatingChange = (rating: number, productId: string) => {
+    setRatings((prevRatings) => ({
+      ...prevRatings,
+      [productId]: rating,
+    }));
+  };
 
-  const handleCambioDeCalificacion = (calificacion: number, id?: string ) => {
-    console.log(calificacion, id);
-  }
+  const handleSendRating = async (productId: string) => {
+    const rating = ratings[productId];
+    if (!rating || !userId) return;
+
+    const ratingPayload = {
+      productId,
+      userId,
+      rating,
+    };
+
+    try {
+      await axios.post("http://localhost:3001/product-rating", ratingPayload);
+      Swal.fire({
+        icon: "success",
+        title: "Calificación Exitosa",
+        text: "¡Gracias por tu calificación!",
+      });
+    } catch (error: any) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.response?.data?.message || "Error al enviar la calificación",
+      });
+    }
+  };
+
   return loading ? (
     <div className="flex items-center justify-center h-screen">
       <Spinner
@@ -54,55 +85,11 @@ const Tracking = ({ params }: { params: { id: string } }) => {
           <div className="flex flex-col md:flex-row md:items-center md:justify-between space-y-3 md:space-y-0 md:space-x-1 p-4 bg-gray-50 border border-gray-200 rounded-t-lg">
             <div className="flex-1 flex items-center space-x-2">
               <h5 className="text-gray-700 font-bold text-center w-full">
-                Detalle de envio
+                Detalle de envío
               </h5>
             </div>
           </div>
           <div className="overflow-x-auto flex flex-col p-4 gap-2">
-            <div className="w-full flex items-center flex-col gap-4">
-              <div className="w-full flex flex-col gap-4 items-center">
-                <h2 className="text-xl font-medium ">Tu pedido:</h2>
-                {order.productsOrder && order.productsOrder.map((product, productIndex) => (
-              <div key={productIndex} className="mb-2 text-start flex items-center">
-                 <Image
-                      width={500}
-                      height={500}
-                      priority={true}
-                      src={product.subproduct.product  ? product.subproduct?.product.imgUrl : ""}
-                      alt={product.subproduct.product ? product.subproduct?.product.description : ""}
-                      className="w-10 h-10 inline-block mr-2 rounded-full"
-                    />
-                    <div className="flex flex-row gap-1">
-                    <span> {product.subproduct.product && product.subproduct?.product.description}</span>
-
-                    <span>  x {product.subproduct?.amount}</span>
-                    <span>  {product.subproduct?.unit}</span>
-                    </div>
-                  </div>
-            ))}
-              </div>
-              <h4>
-                Tu orden fue realizada el:{" "}
-                <b className="font-bold">{format(new Date(order.date), "d 'de' MMMM 'de' yyyy", {
-                  locale: es,
-                })}
-                    </b> 
-                
-              </h4>
-           
-              {order.orderDetail.transactions.status !== "Recibido" && <p>Tu orden llegara a tu domicilio antes del:{" "}
-                      <b className="font-bold">
-                      {format(
-                        new Date(order.orderDetail.deliveryDate),
-                        "d 'de' MMMM 'de' yyyy",
-                        {
-                          locale: es,
-                        }
-                      )}
-                    </b> </p> }
-
-            </div>
-            
             <div className="w-full flex justify-start items-center py-20">
               <div className="flex gap-4">
                 <TrackingComponent1
@@ -122,81 +109,100 @@ const Tracking = ({ params }: { params: { id: string } }) => {
                       >
                         {status}
                       </h1>
-                      <div className="w-1/2 text-center ">
-                      {status === order.orderDetail.transactions.status && (
-                      status === "Recibido" ? (
-                       
-                          <p>
-                            El local confirmo tu pedido, la fecha de entrega es:{" "}
-                            <b className="font-bold">
-                              {format(
-                                new Date(order.orderDetail.deliveryDate),
-                                "d 'de' MMMM 'de' yyyy",
-                                {
-                                  locale: es,
-                                }
-                              )}
-                            </b>
-                          </p>
-                        
-                      ) : status === "Empaquetado" ? (
-                       
-                        <p>
-                          El local ya preparo tu pedido, la fecha de entrega es:{" "}
-                          <b className="font-bold">
-                            {format(
-                              new Date(order.orderDetail.deliveryDate),
-                              "d 'de' MMMM 'de' yyyy",
-                              {
-                                locale: es,
-                              }
-                            )}
-                          </b>
-                        </p>
-                      
-                    ): status === "Transito" ? (
-                       
-                      <p>
-                        Tu pedido se encuentra en camino :{" "}
-                        <b className="font-bold">
-                          hoy llega a tu domicilio
-                        </b>
-                      </p>
-                    
-                  ): status === "Entregado" ? (
-                       
-                    <p>
-                      Tu pedido ya fue entregado.
-                      <b className="font-bold">
-                        Ayudanos y comenta que te parecio el producto.
-                        {order.productsOrder && order.productsOrder.map((product, productIndex) => (
-              <div key={productIndex} className="mb-2 text-start flex items-center gap-4">
-                 <Image
-                      width={500}
-                      height={500}
-                      priority={true}
-                      src={product.subproduct.product  ? product.subproduct?.product.imgUrl : ""}
-                      alt={product.subproduct.product ? product.subproduct?.product.description : ""}
-                      className="w-10 h-10 inline-block mr-2 rounded-full"
-                    />
-                    <div className="flex flex-row gap-1 items-center" >
-                    <span className="text-nowrap"> {product.subproduct.product && product.subproduct?.product.description}</span>
-
-                    <span className="text-nowrap">  x {product.subproduct?.amount}</span>
-                    <span className="text-nowrap">  {product.subproduct?.unit}</span>
-                    
-                    </div>
-                    <RatingStars onChange={handleCambioDeCalificacion } id={product.id}/>
-                  </div>
-            ))}
-                      </b>
-                    </p>): null)}</div>
+                      <div className="w-1/2 text-center">
+                        {status === order.orderDetail.transactions.status &&
+                          (status === "Entregado" && (
+                            <p>
+                              Tu pedido ya fue entregado.
+                              <b className="font-bold">
+                                Ayúdanos a calificar los productos.
+                              </b>
+                            </p>
+                          ))}
+                      </div>
                     </div>
                   ))}
                 </div>
               </div>
             </div>
+            <h4>
+              Tu orden fue realizada el:{" "}
+              <b className="font-bold">
+                {format(new Date(order.date), "d 'de' MMMM 'de' yyyy", {
+                  locale: es,
+                })}
+              </b>
+            </h4>
+            {order.orderDetail.transactions.status !== "Recibido" && (
+              <p>
+                Tu orden llegará a tu domicilio antes del:{" "}
+                <b className="font-bold">
+                  {format(
+                    new Date(order.orderDetail.deliveryDate),
+                    "d 'de' MMMM 'de' yyyy",
+                    {
+                      locale: es,
+                    }
+                  )}
+                </b>
+              </p>
+            )}
           </div>
+          
+          {/* Aquí movemos la sección de calificación al final */}
+          <div className="p-4">
+            <h2 className="text-xl font-medium mb-4">Califica los productos:</h2>
+            {order.productsOrder &&
+              order.productsOrder.map((product, productIndex) => (
+                <div
+                  key={productIndex}
+                  className="mb-6 text-start flex flex-col gap-4 p-4 border-b border-gray-300"
+                >
+                  <div className="flex items-center gap-4">
+                    <Image
+                      width={80}
+                      height={80}
+                      priority={true}
+                      src={
+                        product.subproduct.product
+                          ? product.subproduct?.product.imgUrl
+                          : ""
+                      }
+                      alt={
+                        product.subproduct.product
+                          ? product.subproduct?.product.description
+                          : ""
+                      }
+                      className="w-16 h-16 rounded-lg"
+                    />
+                    <div className="flex flex-col">
+                      <span>
+                        {product.subproduct.product &&
+                          product.subproduct?.product.description}
+                      </span>
+                      <span>
+                        x {product.subproduct?.amount} {product.subproduct?.unit}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="mt-4">
+                    <RatingStars
+                      onChange={(rating) =>
+                        handleRatingChange(rating, product?.subproduct?.product?.id ?? "")
+                      }
+                      id={product.id}
+                    />
+                  </div>
+                  <button
+                    className="mt-2 bg-teal-500 text-white px-4 py-2 rounded-lg"
+                    onClick={() => handleSendRating(product.subproduct.product?.id ?? "")}
+                  >
+                    Envía tu Calificación
+                  </button>
+                </div>
+              ))}
+          </div>
+
           <div className="flex justify-start w-full items-center border-t-gray-300 border h-20 px-10 bg-gray-100">
             <Link
               href={"/dashboard/cliente/order"}
