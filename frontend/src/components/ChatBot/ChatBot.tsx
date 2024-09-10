501a4f0f640897008229c2d75a408868b38f771a
Julian Magallanes <100171315+Julian-Magallanes@users.noreply.github.com>
2024-09-10 11:43:13 -0300

    continuando con chatbot

--- frontend/src/components/ChatBot/ChatBot.tsx
+++ frontend/src/components/ChatBot/ChatBot.tsx
@@ -1,8 +1,70 @@
 "use client";
 
+import { useAuthContext } from "@/context/auth.context";
+import { useRouter } from "next/navigation";
 import ChatBot from "react-chatbotify";
-
+const urlLocal = process.env.NEXT_PUBLIC_URL_LOCAL;
 const ChatBotEsmeralda = () => {
+  const {session} = useAuthContext();
+  const router = useRouter();
+  const helpOptions = ["Quiero comprar", "Quiero ver ofertas", "Quiero iniciar sesion", "Quiero ver mis ordenes", "Quiero hablar con una persona"];
+  const flow = {
+    
+    start: {
+      message: session?.name ? `Hola ${session.name}!` : "Hola!",
+      transition: {duration: 1000},
+      path: "step2"
+      
+    },
+    step2: {
+      message: "¿Que quieres hacer?",
+      options: helpOptions,
+      path: "process_options"
+    },
+
+    process_options: {
+      transition: {duration: 0},
+      chatDisabled: true,
+      path: async (params: any) => {
+        let link = "";
+        switch (params.userInput) {
+        case "Quiero comprar":
+          router.push (urlLocal + "/categories");
+          await params.injectMessage("Ya estas aca, elige lo que quieras.");
+          break;
+        case "Quiero ver ofertas":
+          router.push (urlLocal + "/promociones");
+          await params.injectMessage("Ya estas aca, elige lo que quieras.");
+          break;
+        case "Quiero iniciar sesion":
+          router.push (urlLocal + "/login");
+          await params.injectMessage("Aca puedes iniciar sesion.");
+          break;
+        case "Quiero ver mis ordenes":
+          router.push (urlLocal + "/dashboard/cliente/order");
+          await params.injectMessage("Aca puedes revisar las ordenes.");
+          break;
+        case "Quiero hablar con una persona":
+          link = "https://api.whatsapp.com/send?phone=541158803709";
+          window.open(link, "_blank");
+          await params.injectMessage("Aca puedes hablar con una persona.");
+          break;
+        default:
+          return "unknown_input";
+        }
+        return "repeat"
+      },
+    },
+    repeat: {
+			transition: {duration: 3000},
+			path: "step2"
+		},
+
+    end: {
+      message: (params: any) => `Hola ${params.userInput}!`,
+      chatDisabled: true
+    }
+  }
   const settings = {
     general: {
       embedded: false,
@@ -17,7 +79,12 @@ const ChatBotEsmeralda = () => {
     },
     botBubble:{
       avatar: "/Recurso1.png",
-    }
+    },
+    tooltip: {
+      text: "Tienes alguna pregunta ?",
+
+    },
+
   }
   
   // styles here
@@ -44,11 +111,12 @@ const ChatBotEsmeralda = () => {
       bottom: '20px',
       right: '210px',
       zIndex: 50,
+      
     },
 
   };
   return (
-    <ChatBot styles={styles} settings={settings} />
+    <ChatBot styles={styles} settings={settings} flow={flow} />
 
   );
 };
