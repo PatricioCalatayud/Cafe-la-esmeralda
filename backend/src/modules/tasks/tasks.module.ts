9fbfd3a4aa39174680b2490e228e3c4765db3cad
ValentinoJC <valenkxdd@gmail.com>
2024-09-12 18:42:31 -0300

    Automatización de recordatorios de orden lunes y jueves

--- backend/src/modules/tasks/tasks.module.ts
+++ backend/src/modules/tasks/tasks.module.ts
@@ -1,14 +1,12 @@
 import { Module } from "@nestjs/common";
 import { OrderModule } from "../order/order.module";
 import { TasksService } from "./tasks.service";
-import { TasksController } from "./tasks.controller";
 import { ScheduleModule } from "@nestjs/schedule";
 import { MailerModule } from "../mailer/mailer.module";
 import { UsersModule } from "../users/users.module";
 
 @Module({
     imports: [ScheduleModule.forRoot(), OrderModule, MailerModule, UsersModule],
-    providers: [TasksService],
-    controllers: [TasksController]
+    providers: [TasksService]
 })
 export class TasksModule {}
--- backend/src/modules/tasks/tasks.service.ts
+++ backend/src/modules/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { orderRepository } from '../order/order.repository';
 import { MailerService } from '../mailer/mailer.service';
 import { UsersService } from '../users/users.service';
@@ -35,19 +35,27 @@ export class TasksService {
         return `Se chequearon las ordenes sin pagar. ${ordersUnpaid.length} ordenes borradas de la base de datos.`;
     }
 
-    @Cron('*/1 * * * *')
+    @Cron('*/1 * * * *') // CronExpression.EVERY_DAY_AT_5PM
     async handleOrderReminder() {
-        const clients = await this.usersService.getClients();
-        const lastSunday = moment().subtract(1, 'week').day(0).add(7, 'days').toDate();
-
-        let sentEmails = 0;
-        for(let client of clients) {
-            if(client.orders.length === 0 || (client.orders[client.orders.length-1].date < lastSunday)) {
-                const emailResponse = await this.mailerService.orderReminder(client.email);
-                if(emailResponse.HttpCode === 200) sentEmails++;
+        const today = new Date();
+        const dayOfWeek = today.getDay(); // busco el día de la semana, por ejemplo lunes = 1 o jueves = 4
+        
+        if(dayOfWeek === 1 || dayOfWeek === 4) {
+            const clients = await this.usersService.getClients();
+    
+            const dayToCompare = new Date(today.getFullYear(), today.getMonth(), today.getDate() - (dayOfWeek)); // día de comparación, comparando hasta el domingo anterior
+    
+            let sentEmails = 0;
+            for(const client of clients) {
+                const lastOrderDate = client.orders.length > 1 ? client.orders[client.orders.length-1].date : null; // sacamos la fecha de la última orden
+    
+                if(!lastOrderDate || lastOrderDate < dayToCompare) {
+                    const emailResponse = await this.mailerService.orderReminder(client.email);
+                    if(emailResponse.HttpCode === 200) sentEmails++;
+                }
             }
+    
+            console.log(`Emails enviados: ${sentEmails}`);
         }
-
-        console.log(`Emails enviados: ${sentEmails}`);
     }
 }
