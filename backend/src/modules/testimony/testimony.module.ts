48b0e0686bfaa2e66added32e539466e2e33282c
Nico <nicolasaddamo1@gmail.com>
2024-09-13 15:52:57 -0300

    testomny to repository

--- backend/src/modules/testimony/testimony.module.ts
+++ backend/src/modules/testimony/testimony.module.ts
@@ -3,10 +3,11 @@ import { TestimonyController } from './testimony.controller';
 import { TestimonyService } from './testimony.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Testimony } from 'src/entities/testimony.entity';
+import { TestimonyRepository } from './testimony.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Testimony])],
   controllers:[TestimonyController],
-  providers: [TestimonyService]
+  providers: [TestimonyService, TestimonyRepository]
 })
 export class TestimonyModule {}
