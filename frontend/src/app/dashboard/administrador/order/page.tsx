80618a0bcdb8c0407553ec9ef2c04c6536a8a506
Patricio Lucas Calatayud <patocalatayud@hotmail.com>
2024-09-23 05:07:51 -0300

    Develop

--- frontend/src/app/dashboard/administrador/order/page.tsx
+++ frontend/src/app/dashboard/administrador/order/page.tsx
@@ -11,7 +11,7 @@ import { getAllOrders, putOrder } from "@/helpers/Order.helper";
 import Image from "next/image";
 import { useAuthContext } from "@/context/auth.context";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faDownload, faX, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faDownload, faX, faEdit, faUpload } from "@fortawesome/free-solid-svg-icons";
 import { Tooltip } from "flowbite-react";
 
 const OrderList = () => {
@@ -40,16 +40,15 @@ const OrderList = () => {
   //! Obtener las Ordenes
   useEffect(() => {
     async function fetchOrders() {
-      const limit = ORDERS_PER_PAGE;
-      const page = currentPage;
       if (token) {
-        const response = await getAllOrders(token);
+        const response: IOrders[] | undefined = await getAllOrders(token);
         if (response) {
-          const orders = response;
-          setOrders(orders);
-          setTotalPages(Math.ceil(orders.length / ORDERS_PER_PAGE));
-          setLoading(false);
+          setOrders(response);
+          setTotalPages(Math.ceil(response.length / ORDERS_PER_PAGE));
+        } else {
+          setOrders([]);
         }
+        setLoading(false);
       }
     }
     if (token) {
@@ -59,7 +58,7 @@ const OrderList = () => {
 
   const onPageChange = (page: number) => setCurrentPage(page);
 
-  //! Función para calcular las ordenes a mostrar en la página actual
+  //! Función para calcular las órdenes a mostrar en la página actual
   const getCurrentPageOrders = () => {
     const filteredOrders = filterOrders();
     const startIndex = (currentPage - 1) * ORDERS_PER_PAGE;
@@ -67,7 +66,7 @@ const OrderList = () => {
     return filteredOrders.slice(startIndex, endIndex);
   };
 
-  //! Función para filtrar las ordenes
+  //! Función para filtrar las órdenes
   const filterOrders = () => {
     if (searchTerm === "") {
       return orders;
@@ -99,22 +98,23 @@ const OrderList = () => {
     setCurrentPage(1);
   };
 
-  //! Función para subir el archivo de factura
+  //! Función para subir un archivo de factura
   const handleUploadFile = async (e: React.ChangeEvent<HTMLInputElement>, orderId: string) => {
     const file = e.target.files?.[0];
     if (file) {
       const formData = new FormData();
       formData.append("file", file);
-
-      // Aquí se envía la petición POST
-      const response = await fetch(`/apiURL/mail/bill`, {
-        method: "POST",
+      formData.append("to", session?.email || ""); // Provide a default value for session?.email
+      formData.append("id", orderId);
+  
+      const response = await fetch(`/apiURL/mail/bill/${orderId}`, {
+        method: "PUT",
         headers: {
           Authorization: `Bearer ${token}`,
         },
         body: formData,
       });
-
+  
       if (response.ok) {
         Swal.fire("¡Archivo subido!", "El archivo ha sido subido correctamente.", "success");
       } else {
@@ -123,143 +123,54 @@ const OrderList = () => {
     }
   };
 
-  //! Función para eliminar el archivo de factura
-  const handleDeleteFile = async (orderId: string) => {
-    // Aquí se envía la petición DELETE
-    const response = await fetch(`/apiURL/mail/bill/${orderId}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      // Aquí irá la información que quieras pasar en el DELETE, si es necesario
-    });
-
-    if (response.ok) {
-      Swal.fire("¡Archivo eliminado!", "El archivo ha sido eliminado correctamente.", "success");
-    } else {
-      Swal.fire("¡Error!", "No se pudo eliminar el archivo.", "error");
-    }
-  };
-
-  //! Función para modificar el archivo de factura
+  //! Función para modificar un archivo de factura
   const handleEditFile = async (orderId: string) => {
-    // Aquí iría el código para seleccionar y modificar el archivo existente
-    Swal.fire("Editar archivo", "Aquí va la lógica para modificar el archivo.", "info");
-    // Aquí iría la lógica PUT
-    const response = await fetch(`/apiURL/mail/bill/${orderId}`, {
-      method: "PUT",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      // Aquí irá la información que quieras pasar en el PUT
-    });
-
-    if (response.ok) {
-      Swal.fire("¡Archivo actualizado!", "El archivo ha sido actualizado correctamente.", "success");
-    } else {
-      Swal.fire("¡Error!", "No se pudo actualizar el archivo.", "error");
-    }
-  };
-
-  //! Función para manejar la verificación de comprobantes
-  const handleTransferOk = async (id: string) => {
-    Swal.fire({
-      title: "¿Estás seguro que el comprobante es correcto?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Sí, Es correcto",
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        const response = await putOrder(id, { transferStatus: "Comprobante verificado", orderStatus: true }, token);
-        if (response && (response?.status === 200 || response?.status === 201)) {
-          setOrders(
-            orders.map((order) =>
-              order.id === id
-                ? {
-                    ...order,
-                    receipt: order.receipt
-                      ? {
-                          ...order.receipt,
-                          status: "Comprobante verificado",
-                        }
-                      : {
-                          id: "",
-                          image: "",
-                          status: "Comprobante verificado",
-                        },
-                  }
-                : order
-            )
-          );
-          Swal.fire("¡Correcto!", "El estado de la orden ha sido actualizado.", "success");
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.onchange = async (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      const file = target.files?.[0];
+      if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("to", session?.email || ""); // Provide a default value for session?.email
+        formData.append("id", orderId);
+  
+        const response = await fetch(`/apiURL/mail/bill/${orderId}`, {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          body: formData,
+        });
+  
+        if (response.ok) {
+          Swal.fire("¡Archivo actualizado!", "El archivo ha sido actualizado correctamente.", "success");
         } else {
-          console.error("Error updating order:", response);
-          Swal.fire("¡Error!", "No se pudo actualizar el estado de la orden.", "error");
+          Swal.fire("¡Error!", "No se pudo actualizar el archivo.", "error");
         }
       }
-    });
+    };
+    fileInput.click();
   };
-
-  //! Función para manejar la denegación de comprobantes
-  const handleTransferReject = async (id: string) => {
-    Swal.fire({
-      title: "¿Estás seguro que el comprobante es incorrecto?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Sí, es incorrecto",
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        const response = await putOrder(id, { transferStatus: "Rechazado" }, token);
-        if (response && (response?.status === 200 || response?.status === 201)) {
-          setOrders(
-            orders.map((order) =>
-              order.id === id
-                ? {
-                    ...order,
-                    receipt: order.receipt
-                      ? {
-                          ...order.receipt,
-                          status: "Rechazado",
-                        }
-                      : {
-                          id: "",
-                          image: "",
-                          status: "Rechazado",
-                        },
-                  }
-                : order
-            )
-          );
-          Swal.fire("¡Correcto!", "El estado de la orden ha sido actualizado.", "success");
-        } else {
-          console.error("Error updating order:", response);
-          Swal.fire("¡Error!", "No se pudo actualizar el estado de la orden.", "error");
-        }
-      }
-    });
-  };
-
-  //! Función para mostrar si hay una factura
+  //! Función para verificar si hay una factura en la orden
   const renderInvoiceColumn = (order: IOrders) => {
-    if (order.invoiceType) {
-      return `Sí - ${order.invoiceType}`;
-    } else {
-      return "No";
-    }
+    return order.bill ? "Sí" : "No";
   };
 
   //! Función para mostrar el manejo de archivos
   const renderFileActionsColumn = (order: IOrders) => {
-    if (order.invoiceType) {
+    if (order.bill) {
       return (
         <div className="flex justify-center items-center gap-4">
-          {order.receipt?.image ? (  // Verificamos si tiene una imagen de recibo
+          {order.bill.imgUrl ? (
             <>
-              <FontAwesomeIcon icon={faCheck} style={{ color: "green" }} />
+              <a href={order.bill.imgUrl} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon
+                  icon={faDownload}
+                  style={{ color: "teal", width: "20px", height: "20px" }}
+                />
+              </a>
               <Tooltip content="Modificar archivo">
                 <button
                   type="button"
@@ -269,19 +180,12 @@ const OrderList = () => {
                   <FontAwesomeIcon icon={faEdit} />
                 </button>
               </Tooltip>
-              <Tooltip content="Eliminar archivo">
-                <button
-                  type="button"
-                  onClick={() => handleDeleteFile(order.id)}
-                  className="py-2 px-3 flex items-center text-sm text-red-600 border-red-600 border rounded-lg hover:bg-red-600 hover:text-white"
-                >
-                  <FontAwesomeIcon icon={faTrash} />
-                </button>
-              </Tooltip>
             </>
           ) : (
             <>
-              <input type="file" onChange={(e) => handleUploadFile(e, order.id)} />
+              <Tooltip content="Subir archivo">
+                <input type="file" onChange={(e) => handleUploadFile(e, order.id)} />
+              </Tooltip>
             </>
           )}
         </div>
@@ -308,10 +212,10 @@ const OrderList = () => {
         "Fecha de pedido - entrega",
         "Lugar de envio",
         "Productos",
-        "Estado", // Menú desplegable
-        "Necesita Factura?", // Mostrar si hay o no factura
-        "Archivo Factura", // Nueva columna para subir, modificar o eliminar archivo
-        "Acciones",
+        "Estado", 
+        "Acciones", 
+        "Necesita Factura?", 
+        "Archivo Factura", 
       ]}
       noContent="No hay Ordenes disponibles"
     >
@@ -365,18 +269,13 @@ const OrderList = () => {
               ))}
           </td>
 
-          {/* Columna Estado: Menú desplegable */}
-          <td
-            className={`px-4 py-3 font-medium text-center ${
-              order.orderDetail?.transactions.status === "Recibido"
-                ? "text-teal-500"
-                : "text-red-500"
-            }`}
-          >
+          {/* Columna Estado */}
+          <td className="px-4 py-3 font-medium text-center">
             <select
               id="status"
               name="status"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              style={{ minWidth: "200px" }} // Ancho mínimo para el select.
               onChange={(e) => handleChange(e, order.id)}
               defaultValue={order.orderDetail?.transactions.status}
             >
@@ -389,7 +288,18 @@ const OrderList = () => {
             </select>
           </td>
 
-          {/* Columna Factura: Si hay factura o no */}
+          {/* Columna Acciones */}
+          <td>
+            {order.receipt && (
+              <div className="flex justify-center items-center gap-4">
+                <div>
+                  {order.receipt.status ? <p className="w-40">{order.receipt.status}</p> : null}
+                </div>
+              </div>
+            )}
+          </td>
+
+          {/* Columna Necesita Factura */}
           <td className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
             {renderInvoiceColumn(order)}
           </td>
@@ -397,43 +307,6 @@ const OrderList = () => {
           {/* Columna Archivo Factura */}
           <td className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
             {renderFileActionsColumn(order)}
-          </td>
-
-          {/* Columna Acciones */}
-          <td>
-            {order.receipt && (
-              <div className="flex justify-center items-center gap-4">
-                <div>
-                  {order.receipt.status ? <p className="w-40">{order.receipt.status}</p> : null}
-                  {order.receipt.image ? (
-                    <a href={order.receipt.image} target="_blank" rel="noopener noreferrer">
-                      <FontAwesomeIcon
-                        icon={faDownload}
-                        style={{ color: "teal", width: "20px", height: "20px" }}
-                      />
-                    </a>
-                  ) : null}
-                </div>
-                <Tooltip content="Aceptar">
-                  <button
-                    type="button"
-                    onClick={() => handleTransferOk(order.id)}
-                    className="py-2 px-3 flex items-center text-sm hover:text-white font-medium text-center text-teal-600 border-teal-600 border rounded-lg hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                  >
-                    <FontAwesomeIcon icon={faCheck} />
-                  </button>
-                </Tooltip>
-                <Tooltip content="Rechazar">
-                  <button
-                    type="button"
-                    onClick={() => handleTransferReject(order.id)}
-                    className="py-2 px-3 flex items-center text-sm hover:text-white font-medium text-center text-red-600 border-red-600 border rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                  >
-                    <FontAwesomeIcon icon={faX} />
-                  </button>
-                </Tooltip>
-              </div>
-            )}
           </td>
         </tr>
       ))}
