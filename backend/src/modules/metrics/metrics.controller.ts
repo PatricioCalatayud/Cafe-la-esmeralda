cb184a2e41f457bbec01ecb117d400eb05859229
Nico <nicolasaddamo1@gmail.com>
2024-09-30 16:23:18 -0300

    final email

--- backend/src/modules/metrics/metrics.controller.ts
+++ backend/src/modules/metrics/metrics.controller.ts
@@ -1,43 +1,43 @@
-import { BadRequestException, Body, Controller, Get } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
 import { OrdersMetricsService } from './metrics.service';
 
 @Controller('metrics')
 export class MetricsController {
     constructor(private readonly ordersMetricsService: OrdersMetricsService) {}
-    @Get('productos-mas-vendidos')
+    @Post('productos-mas-vendidos')
     async getMostSoldProducts(
         @Body('limit') limit: number
     ) {
         return await this.ordersMetricsService.getMostSoldProductsService(limit);
     }
-    @Get('productos-menos-vendidos')
+    @Post('productos-menos-vendidos')
     async getLessSoldProducts(
         @Body('limit') limit: number
     ) {
         return await this.ordersMetricsService.getLessSoldProductsService(limit);
     }
 
-    @Get('mejores-productos')
+    @Post('mejores-productos')
     async getBestProducts(
         @Body('limit') limit: number
     ) {
         return await this.ordersMetricsService.getBestProductsService(limit);
     }
-    @Get('peores-productos')
+    @Post('peores-productos')
     async getWorstProducts(
         @Body('limit') limit: number
     ) {
         return await this.ordersMetricsService.getWorstProductsService(limit);
     }
 
-    @Get('deudores')
+    @Post('deudores')
     async getLargestDebtors(
         @Body('limit') limit: number
     ) {
         return await this.ordersMetricsService.getLargestDebtorsService(limit);
     }
 
-    @Get('pedidos-usuario-mes')
+    @Post('pedidos-usuario-mes')
     async getOrdersByUserIdAndDate(
         @Body('id') id: string,
         @Body('date') date: string
@@ -51,7 +51,7 @@ export class MetricsController {
         return await this.ordersMetricsService.getOrdersByUserIdAndDateService(id, dateSelected);
     }
 
-    @Get('productos-por-mes')
+    @Post('productos-por-mes')
     async getProductsByMonth(
         @Body('date') date: string,
         @Body('productId') productId: string,
@@ -66,7 +66,7 @@ export class MetricsController {
         return await this.ordersMetricsService.getProductsByMonthService(dateSelected, productId, limit);
     }
     
-    @Get('productos-vendidos')
+    @Post('productos-vendidos')
     async geAllTimeProducts(
         @Body('productId') productId: string,
         @Body('limit') limit: number
