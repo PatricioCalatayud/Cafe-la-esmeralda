40d0cebacd9a69e45a3d109e04997efa3c96ba86
Patricio Lucas Calatayud <patocalatayud@hotmail.com>
2024-10-22 17:30:32 -0300

    Commit

--- backend/src/config/mailer.config.ts
+++ backend/src/config/mailer.config.ts
@@ -3,18 +3,18 @@ import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig({ path: '.env.development'});
 
-export const email = 'tech@robolsolutions.com';
-// export const email = 'Informes@cafelaesmeralda.com.ar';
+export const email = 'Informes@cafelaesmeralda.com.ar';
 
 const transporter = nodemailer.createTransport({
-    // host: 'imap.cafelaesmeralda.com.ar',
-    host: 'smtp.hostinger.com',
+    host: 'mxout3.hostingtelefonica.com.ar',
     port: 587,
     secure: false,
     auth: {
         user: email,
         pass: process.env.PASS_EMAIL
-    }
+    },
+    
+    
 })
 
 export default transporter;
