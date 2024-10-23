fc4607ff9a73cf483322632d6e03428994544f3c
Patricio Lucas Calatayud <patocalatayud@hotmail.com>
2024-10-23 13:53:39 -0300

    Correcciones e2e dashboards 1

--- frontend/src/components/DashboardComponent/DashboardProfile.tsx
+++ frontend/src/components/DashboardComponent/DashboardProfile.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useAuthContext } from "@/context/auth.context";
-import { putUser, putAddress } from "@/helpers/Autenticacion.helper"; // putAddress para la llamada PUT de la dirección
+import { putAddress } from "@/helpers/Autenticacion.helper";
 import { ISession } from "@/interfaces/ISession";
 import { faCheck, faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,14 +8,46 @@ import { Tooltip } from "flowbite-react";
 import { useState } from "react";
 import Swal from "sweetalert2";
 
+// Mapeo de provincias (lo puedes extraer desde la vista de registro o mantenerlo aquí)
+const provinceMapping: Record<number, string> = {
+  1: "Buenos Aires",
+  2: "Córdoba",
+  3: "Catamarca",
+  4: "Chaco",
+  5: "Chubut",
+  6: "Corrientes",
+  7: "Entre Ríos",
+  8: "Formosa",
+  9: "Jujuy",
+  10: "La Pampa",
+  11: "La Rioja",
+  12: "Mendoza",
+  13: "Misiones",
+  14: "Neuquén",
+  15: "Río Negro",
+  16: "Salta",
+  17: "San Juan",
+  18: "San Luis",
+  19: "Santa Cruz",
+  20: "Santa Fe",
+  21: "Santiago del Estero",
+  22: "Tierra del Fuego",
+  23: "Tucumán",
+  24: "Ciudad Autónoma de Buenos Aires",
+};
+
 const DashboardProfile = ({ session }: { session?: ISession }) => {
   const { token } = useAuthContext();
   const { setSession } = useAuthContext();
 
-  // Estados locales para el teléfono y la dirección
+  // Estados locales para el teléfono, la dirección y la localidad
   const [phone] = useState<string>(session?.phone || "");
   const [address, setAddress] = useState<string>(session?.address?.address || "");
+  const [locality, setLocality] = useState<string>(session?.address?.localidad || "");
+  const [province, setProvince] = useState<number>(session?.address?.province || 0);
   const [editAddress, setEditAddress] = useState<boolean>(false);
+  const [editLocality, setEditLocality] = useState<boolean>(false);
+  const [editProvince, setEditProvince] = useState<boolean>(false);
 
   // Función para editar la dirección
   const handleEditAddress = async () => {
@@ -28,10 +60,15 @@ const DashboardProfile = ({ session }: { session?: ISession }) => {
       return;
     }
 
-    // Verificamos que el id de la dirección exista antes de hacer la petición PUT
     if (session?.address?.id) {
-      // Llamada PUT a la API para actualizar la dirección
-      const response = await putAddress(session?.address?.id, { address }, token);
+      const updatedAddress = {
+        address,
+        localidad: locality,
+        province, // Guardar la provincia como número
+        deliveryNumber: session.address.deliveryNumber || 0,
+      };
+
+      const response = await putAddress(session?.address?.id, updatedAddress, token);
 
       if (response && (response.status === 200 || response.status === 201)) {
         Swal.fire({
@@ -39,9 +76,10 @@ const DashboardProfile = ({ session }: { session?: ISession }) => {
           title: "¡Éxito!",
           text: "Se ha actualizado tu dirección correctamente",
         });
-        // Actualizamos la sesión solo si el id de la dirección es válido
-        setSession({ ...session, address: { ...session.address, address } });
+        setSession({ ...session, address: { ...session.address, address, localidad: locality, province } });
         setEditAddress(false);
+        setEditLocality(false);
+        setEditProvince(false);
       } else {
         Swal.fire({
           icon: "error",
@@ -60,7 +98,7 @@ const DashboardProfile = ({ session }: { session?: ISession }) => {
 
   return (
     <section className="p-1 sm:p-1 antialiased h-screen dark:bg-gray-700">
-      <div className="w-full ">
+      <div className="w-full">
         <div className="bg-white dark:bg-gray-800 relative shadow-2xl sm:rounded-lg overflow-hidden">
           <div className="flex flex-col md:flex-row md:items-center md:justify-between space-y-3 md:space-y-0 md:space-x-1 p-4 bg-gray-50 border border-gray-200 rounded-t-lg">
             <div className="flex-1 flex items-center space-x-2">
@@ -71,12 +109,12 @@ const DashboardProfile = ({ session }: { session?: ISession }) => {
           </div>
 
           <div className="overflow-x-auto flex flex-col p-4 gap-2">
-            <h2>Name: {session?.name}</h2>
+            <h2>Nombre: {session?.name}</h2>
             <h2>Email: {session?.email}</h2>
 
             {/* Teléfono */}
             <div className="flex items-center justify-start gap-2">
-              <h2 className="text-nowrap">Phone: </h2>
+              <h2 className="text-nowrap">Teléfono: </h2>
               <p>{session?.phone}</p>
             </div>
 
@@ -120,16 +158,89 @@ const DashboardProfile = ({ session }: { session?: ISession }) => {
               )}
             </div>
 
-            {/* Localidad (no editable) */}
+            {/* Localidad (editable) */}
             <div className="flex items-center justify-start gap-2">
               <h2 className="text-nowrap">Localidad: </h2>
-              <p>{session?.address?.localidad}</p>
+              {editLocality ? (
+                <>
+                  <input
+                    type="text"
+                    name="locality"
+                    id="locality"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Nueva localidad"
+                    value={locality}
+                    onChange={(e) => setLocality(e.target.value)}
+                  />
+                  <Tooltip content="Guardar">
+                    <button
+                      type="button"
+                      onClick={handleEditAddress}
+                      className="py-3 px-3.5 flex items-center text-sm hover:text-white font-medium text-center text-teal-600 border-teal-600 border rounded-lg hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                      <FontAwesomeIcon icon={faCheck} />
+                    </button>
+                  </Tooltip>
+                </>
+              ) : (
+                <>
+                  <p>{session?.address?.localidad}</p>
+                  <Tooltip content="Editar">
+                    <button
+                      type="button"
+                      onClick={() => setEditLocality(true)}
+                      className="py-3 px-3.5 flex items-center text-sm hover:text-white font-medium text-center text-teal-600 border-teal-600 border rounded-lg hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                      <FontAwesomeIcon icon={faPen} />
+                    </button>
+                  </Tooltip>
+                </>
+              )}
             </div>
 
-            {/* Provincia (no editable) */}
+            {/* Provincia (editable) */}
             <div className="flex items-center justify-start gap-2">
               <h2 className="text-nowrap">Provincia: </h2>
-              <p>{session?.address?.province}</p>
+              {editProvince ? (
+                <>
+                  <select
+                    name="province"
+                    id="province"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    value={province}
+                    onChange={(e) => setProvince(Number(e.target.value))}
+                  >
+                    {Object.keys(provinceMapping).map((key) => (
+                      <option key={key} value={key}>
+                        {provinceMapping[Number(key)]}
+                      </option>
+                    ))}
+                  </select>
+                  <Tooltip content="Guardar">
+                    <button
+                      type="button"
+                      onClick={handleEditAddress}
+                      className="py-3 px-3.5 flex items-center text-sm hover:text-white font-medium text-center text-teal-600 border-teal-600 border rounded-lg hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                      <FontAwesomeIcon icon={faCheck} />
+                    </button>
+                  </Tooltip>
+                </>
+              ) : (
+                <>
+                  {/* Muestra el nombre de la provincia usando el mapeo */}
+                  <p>{provinceMapping[session?.address?.province || 0]}</p>
+                  <Tooltip content="Editar">
+                    <button
+                      type="button"
+                      onClick={() => setEditProvince(true)}
+                      className="py-3 px-3.5 flex items-center text-sm hover:text-white font-medium text-center text-teal-600 border-teal-600 border rounded-lg hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                      <FontAwesomeIcon icon={faPen} />
+                    </button>
+                  </Tooltip>
+                </>
+              )}
             </div>
           </div>
 
