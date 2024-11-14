c6327f50015d3e2624f39811b8a70e194e26c1a3
Patricio Lucas Calatayud <patocalatayud@hotmail.com>
2024-11-14 13:16:37 -0300

    Fixinig .2

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -564,24 +564,24 @@ const total = calcularTotal();
               </div>
             )}
 
-            {/* Input de DNI habilitado solo para Factura B o C */}
-            {(invoiceType === "B" || invoiceType === "C") && (
-              <div className="w-full h-20 gap-4 flex flex-col">
-                <label htmlFor="dni" className="block text-sm font-medium text-gray-900 dark:text-white">
-                  DNI
-                </label>
-                <input
-                  type="text"
-                  name="dni"
-                  id="dni"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Ejemplo: 12345678"
-                  value={dni}
-                  onChange={(e) => setDni(e.target.value)}
-                  disabled={invoiceType === "A"}
-                />
-              </div>
-            )}
+         {/* Input de DNI habilitado solo para Factura B o C */}
+{(invoiceType === "B" || invoiceType === "C") && (
+  <div className="w-full h-20 gap-4 flex flex-col">
+    <label htmlFor="dni" className="block text-sm font-medium text-gray-900 dark:text-white">
+      DNI
+    </label>
+    <input
+      type="text"
+      name="dni"
+      id="dni"
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+      placeholder="Ejemplo: 12345678"
+      value={dni}
+      onChange={(e) => setDni(e.target.value)}
+      disabled={invoiceType !== "B" && invoiceType !== "C"} // Cambiado aquí
+    />
+  </div>
+)}
           </div>
         )}
       </>
